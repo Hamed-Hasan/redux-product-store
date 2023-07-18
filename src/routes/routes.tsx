784474e36d4d4fb0fd9e7db1fb1d18c7f216325e
John Doe <hamedhasan422421@gmail.com>
2023-07-18 14:48:07 +0300

    - Update: Import the createBrowserRouter function from the 'react-router-dom' library. - Update: Import the App component from the '@/App' module. - Update: Import the NotFound, Home, Products, Checkout, ProductDetails, LoginForm, SignupForm, and PrivateRoute components from their respective modules. - Create a routing configuration using the createBrowserRouter function. - Configure the routes with the following structure:   - The root path '/' is associated with the App component.   - The root path '/' has child routes:     - The index route is associated with the Home component.     - The '/products' path is associated with the Products component.     - The '/product-details/:id' path is associated with the ProductDetails component.     - The '/checkout' path is associated with the Checkout component, wrapped in the PrivateRoute component to protect the route.   - The '/login' path is associated with the LoginForm component.   - The '/signup' path is associated with the SignupForm component.   - The '*' path (wildcard) is associated with the NotFound component, which will be displayed for any unknown paths. - Export the routing configuration.

---
+++ src/routes/routes.tsx
@@ -0,0 +1,58 @@
+import { createBrowserRouter } from 'react-router-dom';
+import App from '@/App';
+
+import NotFound from '@/pages/NotFound';
+import Home from '@/pages/Home';
+import Products from '@/pages/Products';
+import Checkout from '@/pages/Checkout';
+
+import ProductDetails from '@/pages/ProductDetails';
+import { LoginForm } from '@/pages/Login';
+import { SignupForm } from '@/pages/Signup';
+import PrivateRoute from './PrivateRoute';
+
+
+
+
+const routes = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: '/products',
+        element: <Products />,
+      },
+      {
+        path: '/product-details/:id',
+        element: <ProductDetails />,
+      },
+      {
+        path: '/checkout',
+        element: (
+          <PrivateRoute>
+            <Checkout/>
+          </PrivateRoute>
+        ),
+      },
+    ],
+  },
+  {
+    path: '/login',
+    element: <LoginForm/>,
+  },
+  {
+    path: '/signup',
+    element: <SignupForm/>,
+  },
+  {
+    path: '*',
+    element: <NotFound />,
+  },
+]);
+
+export default routes;
